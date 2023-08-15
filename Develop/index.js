e681f4427af146e5087683b83ed7493a13af1562
cip170 <charlottephilipps@gmail.com>
2023-08-15 19:24:01 +1000

    adding initial code in index.js & adding output file for readme destination

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,29 @@
 // TODO: Include packages needed for this application
+// Declaring variables in order to use inquirer & fs
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const readMe = (data) => 
+`# ${data.title}`
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+.prompt(
+    [
+        {type: 'input',
+        message: 'Enter a project title',
+        name: 'title',
+    }]
+)
 
+.then((data) => {
+    const readMeContent = readMe(data);
+    fs.writeFile('./Output/README.md', readMeContent, (err) =>
+    err ? console.error(err) : console.log('README file created')
+    );
+});
+
+/*
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -11,3 +32,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+*/
